fd09d3381f1b32b27326a7ce0ce0a8bdd2f17fba
juhairulislam <juhairulislam2018@gmail.com>
2026-02-28 23:27:21 +0600

    add destructure of array and object

--- destructure.js
+++ destructure.js
@@ -1,2 +1,32 @@
-const {name} = {name: 'Dr. Juhairul'} ;
-console.log(name)
+// const {name} = {name: 'Dr. Juhairul'} ;
+// console.log(name)
+
+
+// arrow destructure 
+
+let num = [1, 2,3,4,5,5] ;
+
+let [ one , two , three ,,,five] = num ;
+console.log(two)
+
+
+
+// Object destructure ....
+
+let person = {
+    name : 'Juhairul Islam' , 
+    roll : 130 ,
+    education : {
+        Aleem : 'TMKM' ,
+        Dakhil : 'Tista' , 
+        Others : {
+            hefz : 'Tista'
+        } 
+
+    }
+}
+
+
+let {name , roll ,education , education:{Aleem, Dakhil, Others, Others:{hefz}}} =person ;
+
+console.log(name , roll , education , Aleem , Dakhil , Others , hefz  )
